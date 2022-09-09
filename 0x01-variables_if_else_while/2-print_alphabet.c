bd0f9ef02bc3269fd59692aec4cc3fdd80dd8e99
omanvic <omanoyevictor@gmail.com>
2022-09-09 13:10:12 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char var;
+
+	for (var = 'a'; var <= 'z'; ++var)
+		putchar(var);
+	return (0);
+}
