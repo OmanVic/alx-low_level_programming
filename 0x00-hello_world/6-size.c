cfa9f3a3105c0c118efc4a9b0122d74f66e496a8
omanvic <omanoyevictor@gmail.com>
2022-09-09 02:21:58 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of long int: %d byte(s)\n", sizeof(long int));
+	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
