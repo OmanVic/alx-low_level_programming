7fbe4e29159e1ff3ac1b8ad5672f61a7d1a710ea
omanvic <omanoyevictor@gmail.com>
2022-09-09 17:41:07 -0700

    futher work with putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int var;
+
+	for (var = '0'; var <= '9'; var++)
+	{
+		putchar(var);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar ('\n');
+	return (0);
+}
