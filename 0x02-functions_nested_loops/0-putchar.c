0e0e2a1bc8593e13f53de644fb3996786c26762b
omanvic <omanoyevictor@gmail.com>
2022-09-13 10:41:37 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char _,p,u,t,c,h,a,r;
+
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	return (0);
+}
