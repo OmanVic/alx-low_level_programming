67cb34f7e691d0364987ea2f0f5387cd1e608761
omanvic <omanoyevictor@gmail.com>
2022-09-14 02:05:57 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,13 @@ void jack_bauer(void)
 {
 	int m, n, p, o;
 
-	for (m = 0; m <= 2; m++)
+	for (m = '0'; m <= '2'; m++)
 	{
-		for (n = 0; n <= 3; n++)
+		for (n = '0'; n <= '3'; n++)
 		{
-			for (p = 0; p <= 5; p++)
+			for (p = '0'; p <= '5'; p++)
 			{
-				for (o = 0; o <= 9; o++)
+				for (o = '0'; o <= '9'; o++)
 				{	_putchar(m);
 					_putchar(n);
 					_putchar(':');
