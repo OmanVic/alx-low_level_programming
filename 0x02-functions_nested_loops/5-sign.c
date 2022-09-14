c446b049f4b0934e41c364614e4dcb928dc2fb6d
omanvic <omanoyevictor@gmail.com>
2022-09-13 21:36:36 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,22 @@
  */
 int print_sign(int n)
 {
+	int test;
 
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		test = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		test = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		test = -1;
 	}
+	return test;
 }
