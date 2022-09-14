e915d181f0cec5c709e81a8334203be0965117b7
omanvic <omanoyevictor@gmail.com>
2022-09-14 10:34:18 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
 		printf("%d\n", n);
 		if (n == 98)
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		}
 		n++;
 	}
-	else if (n > 98)
+	while (n > 98)
 	{
 		printf("%d\n", n);
 		if (n == 98)
