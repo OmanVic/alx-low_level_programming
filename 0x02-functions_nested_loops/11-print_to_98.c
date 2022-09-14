2bbfc0f8e3dbe696f860827fe8e322db29bf55a4
omanvic <omanoyevictor@gmail.com>
2022-09-14 09:40:57 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_to_98 - printing numbers to digit 98
+ *
+ * @n: starting number
+ *
+ */
+void print_to_98(int n)
+{
+	do {
+		printf("%d", n);
+		n++
+			if (n > 98)
+			{
+				break;
+			}
+	} while (n < 98);
+}
