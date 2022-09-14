8d897a3c135badacd746e0589bd171b98afa8bf3
omanvic <omanoyevictor@gmail.com>
2022-09-13 17:02:32 -0700

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check if etter is in lowercase
+ *
+ * Return: 0 if c is lower
+ *
+ * Return: 1 otherwise
+ *
+ */
+int _islower(int c)
+{
+	return(c>=97 && c<=122);
+}
