8f900a18964b1eaba4b4d026fcb887911f3938ce
omanvic <omanoyevictor@gmail.com>
2022-09-15 16:33:42 -0700

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,21 +9,21 @@
  */
 void print_square(int size)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= size; j++)
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
 		{
-			if (size <= 0)
-			{
-				_putchar('\n');
-			}
-			else
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
