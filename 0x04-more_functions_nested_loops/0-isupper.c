5f5fda39207102b0fb3ef8cc10f4acea502115b5
omanvic <omanoyevictor@gmail.com>
2022-09-15 10:51:53 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - check the case of an alphabet
+ *
+ * Return: 0 or 1
+ *
+ * @c: variable to check
+ *
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
