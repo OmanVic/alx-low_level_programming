9e5002d6d15705451f178403522b0cea71792f85
omanvic <omanoyevictor@gmail.com>
2022-09-16 12:36:50 -0700

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - the starting of a program
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if ((i % 3 != 0) || (i % 5 != 0))
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
