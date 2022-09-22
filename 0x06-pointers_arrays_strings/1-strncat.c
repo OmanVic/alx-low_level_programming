9c1419bb9ba7e580574cea4e672def1faec9b228
omanvic <omanoyevictor@gmail.com>
2022-09-22 14:08:56 -0700

    working on the terminting null character

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,11 +21,12 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	int j = 0;
 
-	while (j < n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
