84540925ca9e8a559449db16542d3ca6429d6ef5
omanvic <omanoyevictor@gmail.com>
2022-09-22 13:57:52 -0700

    writing the logic for a strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,5 +8,25 @@
  *
  * @n: number
  *
+ * @src: second string
+ *
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
 
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	int j = 0;
+
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,4 +1,4 @@
-#include "0-strcat.c"
+#include "1-strncat.c"
 #include <stdio.h>
 
 /**
@@ -14,7 +14,11 @@ int main(void)
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strcat(s1, s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
