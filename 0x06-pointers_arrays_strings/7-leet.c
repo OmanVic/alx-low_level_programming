02bd2f9ac36f92c9544727c44522d4fbe4533192
omanvic <omanoyevictor@gmail.com>
2022-09-23 14:25:20 -0700

    a function to encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - a function to encode a string
+ *
+ * @n: input variable
+ *
+ * Return: n
+ *
+ */
+char *leet(char *n)
+{
+	char *x = "AaEeOoTtLl";
+
+	char *y = "4433007711";
+
+	int i, j = 0;
+
+	while (n[i] != '\0')
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			if (n[i] == x[j])
+			{
+				n[i] = y[j];
+			}
+		}
+		i++;
+	}
+	return (n);
+}
