4200665aadeeff936173936d4f51efd018c6ad6d
omanvic <omanoyevictor@gmail.com>
2022-10-06 00:50:44 -0700

    hot fixing return error

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,45 @@
-#include "main.h"
+int prime(int n, int i);
 /**
- * is_prime_number - a function to check for natural prime number
+ * is_prime_number - a function to check for prime numbers
  *
- * @n: integer to be checked
+ * @n: input variable
  *
- * Return: 1 if prime or 0 if not
+ * Return: 1 (if n is a prime number) or 0 (if n is not)
  *
- **/
+ */
 int is_prime_number(int n)
 {
-	static int i = 1;
-
-	++i;
-	if ((n < 2 || n % i == 0) && n != i)
+	if (n < 2)
 	{
-		return 0;
-	}
-	else if (n == i)
-	{
-		return 1;
+		return (0);
 	}
 	else
 	{
-		return is_prime_number(n);
+		return (prime(n, 2));
+	}
+}
+/**
+ * prime - a function to text for prime numbers
+ *
+ * @n: integer to check
+ *
+ * @i: incremental factor
+ *
+ * Return: 1 or 0
+ *
+ */
+int prime(int n, int i)
+{
+	if ((n % i == 0) && (i != 0 || i != 1) && (n != i))
+	{
+		return (0);
+	}
+	else if (n == i)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime(n, ++i));
 	}
 }
