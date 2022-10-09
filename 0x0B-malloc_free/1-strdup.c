86af1156b77be43ead0b08c02c40182907815f4e
omanvic <omanoyevictor@gmail.com>
2022-10-08 17:18:04 -0700

    a function to duplicate a string using mallox

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - a function to duplicate a string
+ *
+ * @str: string to be duplicated
+ *
+ * Return: a pointer
+ *
+ */
+char *_strdup(char *str)
+{
+	char *p;
+
+	unsigned int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (str[i] < '\0')
+	{
+		j++;
+		i++;
+	}
+
+	p = (char *) malloc(sizeof(char) * (j + 1));
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
