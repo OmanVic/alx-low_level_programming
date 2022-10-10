f94d030aa22c652a71398de32467342e60ae6d22
omanvic <omanoyevictor@gmail.com>
2022-10-10 08:53:47 -0700

    creating a header file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structute to keep record of dog info
+ *
+ * @name: name of dog
+ *
+ * @age: age of dog
+ *
+ * @owner: owner of dog
+ *
+ * Description: Showing the name,age and owners of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
