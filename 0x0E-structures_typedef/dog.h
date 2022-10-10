3ad00e4062ebc757a8485e879013e06a90004714
omanvic <omanoyevictor@gmail.com>
2022-10-10 14:55:19 -0700

    using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,13 @@
  *
  * Description: Showing the name,age and owners of dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
