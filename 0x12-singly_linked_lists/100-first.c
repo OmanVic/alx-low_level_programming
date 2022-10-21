30f3d096c18fdeb98ea944ea99e874ca8d277013
omanvic <omanoyevictor@gmail.com>
2022-10-20 20:54:59 -0700

    printf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * printf - to print strings
+ *
+ * Return: void
+ *
+ */
+void printf(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
