80708564084a4bca09a278f8e8617b02aaf5fc27
omanvic <omanoyevictor@gmail.com>
2022-10-26 13:20:02 -0700

    getting a particular node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function to get a node
+ *
+ * @head: pointer to a variable
+ *
+ * @index: index
+ *
+ * Return: pointer to index n
+ *
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	listint_t *ptr = head;
+
+	for (i = 0; i < index; i++)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
