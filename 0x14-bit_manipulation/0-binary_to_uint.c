ce1ce79b4ba45f459ff0e4decd2196aaab698a05
omanvic <omanoyevictor@gmail.com>
2022-10-28 19:49:34 -0700

    code update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,8 +13,8 @@ unsigned int binary_to_uint(const char *b)
 
 	val = i = 0;
 	if (b == NULL)
-		return 0;
-	while ( b[i] == '0' || b[i] == '1')
+		return (0);
+	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
 		val += b[i] - '0';
