e7869d30f6c116890965fe613b01ae9ce4b38fcf
omanvic <omanoyevictor@gmail.com>
2022-10-28 19:41:12 -0700

    converting binary to unsigned integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - a function to covert binary to base 10
+ *
+ * @b: pointer to a variable
+ *
+ * Return: decimal number or 0
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val, i;
+
+	val = i = 0;
+	if (b == NULL)
+		return 0;
+	while ( b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
+
+}
+
